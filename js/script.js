9e4e95c4e1d96712ee6edc82c1c0633e6560397d
sisdream <sissipascale2@gmail.com>
2024-01-10 17:44:42 +0100

    feat: js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const userName = document.getElementById ("user-name");
+const path = document.getElementById ("path");
+const userAge = document.getElementById ("user-age");
+const genBtn = document.getElementById ("gen-btn");
+const btnUser = document.getElementById('btn-user')
+const hiddenTicket = document.getElementById("hidden-ticket")
+const price = document.getElementById('price')
+
+
+
+// per mettere in display-none la section biglietto
+bigliettoNascostoHtml.style.display = "none"
+

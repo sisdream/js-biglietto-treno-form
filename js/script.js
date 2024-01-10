170b2066eb2333f8ecfaa46c9fc80068021a2422
sisdream <sissipascale2@gmail.com>
2024-01-10 18:52:21 +0100

    fix: js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
-const userName = document.getElementById ("user-name");
-const path = document.getElementById ("path");
-const userAge = document.getElementById ("user-age");
-const genBtn = document.getElementById ("gen-btn");
-const btnUser = document.getElementById('btn-user')
-const hiddenTicket = document.getElementById("hidden-ticket")
-const price = document.getElementById('price')
+const userName = document.getElementById("user-name");
+const path = document.getElementById("path");
+const userAge = document.getElementById("user-age");
+const genBtn = document.getElementById("gen-btn");
+const annBtn = document.getElementById("ann-btn");
+const btnUser = document.getElementById('btn-user');
+const hiddenTicket = document.getElementById("hidden-ticket");
+const price = document.getElementById('price');
 
 // per mettere in display-none la section biglietto
-hiddenTicket.style.display = "none"
-
+hiddenTicket.style.display = "none";
+const pricePerKm = 0.21;
 
 // // al click del bottone attiva
-genBtn.addEventListener( 'click', function() {
+genBtn.addEventListener('click', function() {
+    // per mettere il nome cognome nel biglietto
+    btnUser.innerHTML = userName.value;
+    let ticketPrice = path.value * pricePerKm;
 
-// per mettere il nome cognome nel biglietto
- btnUser.innerHTML = userName.value
-if( userAge.value === "minorenne" ){
-    price.innerHTML = (path.value * 0.21) * 0.8
-  
-}else if( userAge.value === "maggiorenne" ){  
- price.innerHTML = (kmHtml.value * 0.21) * 0.2
-  
-}else if( userAge.value === "over65" ){  
-    price.innerHTML = (kmHtml.value * 0.21) * 0.6
-}
+    if (userAge.value === "minorenne") {
+        ticketPrice = (ticketPrice * 20) / 100;
+    } else if (userAge.value === "over65") {
+        ticketPrice = (ticketPrice * 40) / 100;
+    }
+
+    price.innerHTML = ticketPrice.toFixed(2) + " EUR";
+    hiddenTicket.style.display = "block";
 });
+
+// al click del bottone annulla
+// rimuovo i valori degli input e nascondo il biglietto
+
+annBtn.addEventListener('click',function(){
+    userName.innerHTML = 0;
+    path
+    
+}
+)

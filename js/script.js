7d32a2a6bc80f467a11b9c1a74045d66e9847f4d
sisdream <sissipascale2@gmail.com>
2024-01-10 17:59:53 +0100

    feat: form

--- js/script.js
+++ js/script.js
@@ -6,8 +6,22 @@ const btnUser = document.getElementById('btn-user')
 const hiddenTicket = document.getElementById("hidden-ticket")
 const price = document.getElementById('price')
 
-
-
 // per mettere in display-none la section biglietto
-bigliettoNascostoHtml.style.display = "none"
+hiddenTicket.style.display = "none"
 
+
+// // al click del bottone attiva
+genBtn.addEventListener( 'click', function() {
+
+// per mettere il nome cognome nel biglietto
+ btnUser.innerHTML = userName.value
+if( userAge.value === "minorenne" ){
+    price.innerHTML = (path.value * 0.21) * 0.8
+  
+}else if( userAge.value === "maggiorenne" ){  
+ price.innerHTML = (kmHtml.value * 0.21) * 0.2
+  
+}else if( userAge.value === "over65" ){  
+    price.innerHTML = (kmHtml.value * 0.21) * 0.6
+}
+});
